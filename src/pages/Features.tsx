d74d50dfe71a598aebbc82d4c6c5661492a84024
nemanjamalesija <malesija13@gmail.com>
2023-04-13 03:44:57 +0200

    Add features section

--- src/pages/Features.tsx
+++ src/pages/Features.tsx
@@ -1,7 +1,50 @@
 const Features = () => {
   return (
     <section className='section-features'>
-      <i className='icon-basic-world'></i>
+      <div className='row row-col-4'>
+        <div className='col-1-of-4'>
+          <div className='feature-box'>
+            <i className='feature-box__icon icon-basic-world'></i>
+            <h3 className='heading-tertiary u-margin-bottom-small'>Explore the world</h3>
+            <p className='feature-box__text'>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa, tempore
+              quia accusantium.
+            </p>
+          </div>
+        </div>
+        <div className='col-2-of-4'>
+          <div className='feature-box'>
+            <i className='feature-box__icon icon-basic-compass'></i>
+            <h3 className='heading-tertiary u-margin-bottom-small'>Meet the nature</h3>
+            <p className='feature-box__text'>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa, tempore
+              quia accusantium.
+            </p>
+          </div>
+        </div>
+        <div className='col-3-of-4'>
+          <div className='feature-box'>
+            <i className='feature-box__icon icon-basic-map'></i>
+            <h3 className='heading-tertiary u-margin-bottom-small'>Find your way</h3>
+            <p className='feature-box__text'>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa, tempore
+              quia accusantium.
+            </p>
+          </div>
+        </div>
+        <div className='col-4-of-4'>
+          <div className='feature-box'>
+            <i className='feature-box__icon icon-basic-heart'></i>
+            <h3 className='heading-tertiary u-margin-bottom-small'>
+              Live a healtier life
+            </h3>
+            <p className='feature-box__text'>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa, tempore
+              quia accusantium.ho
+            </p>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
