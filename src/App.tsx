e921f52a008a5338c88feeeb846975c62a833fb7
nemanjamalesija <malesija13@gmail.com>
2023-04-15 00:34:22 +0200

    Add footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Header from './components/Header';
 import About from './sections/About';
 import BookForm from './sections/BookForm';
 import Features from './sections/Features';
+import Footer from './sections/Footer';
 import Testimonials from './sections/Testimonials';
 import Tours from './sections/Tours';
 
@@ -14,6 +15,7 @@ function App() {
       <Tours />
       <Testimonials />
       <BookForm />
+      <Footer />
     </div>
   );
 }
---
+++ src/sections/Footer.tsx
@@ -0,0 +1,63 @@
+import logoGreen2x from '../img/logoGreen2x.png';
+
+const Footer = () => {
+  return (
+    <footer className='footer'>
+      <div className='footer__logo-box'>
+        <img
+          src={logoGreen2x}
+          alt='Full logo'
+          className='footer__logo u-margin-bottom-big'
+        />
+      </div>
+      <div className='row row__col--2 '>
+        <div className='col-footer col-1-of-2'>
+          <div className='footer__navigation'>
+            <ul className='footer__list'>
+              <li className='footer__item'>
+                <a href='#' className='footer__link'>
+                  Company
+                </a>
+              </li>
+              <li className='footer__item'>
+                <a href='#' className='footer__link'>
+                  Contact us
+                </a>
+              </li>
+              <li className='footer__item'>
+                <a href='#' className='footer__link'>
+                  Careers
+                </a>
+              </li>
+              <li className='footer__item'>
+                <a href='#' className='footer__link'>
+                  Privacy
+                </a>
+              </li>
+              <li className='footer__item'>
+                <a href='#' className='footer__link'>
+                  Terms
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className='col-footer col-2-of-2'>
+          <p className='footer__copyright'>
+            Built by &nbsp;
+            <a href='#' className='footer__link'>
+              Nemanja Malesija &nbsp;
+            </a>
+            for his SAAS practice project &nbsp;
+            <a href='#' className='footer__link'>
+              Natours &nbsp;
+            </a>
+            Advanced CSS and SASS course by Jonas Schmedtmann..
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
