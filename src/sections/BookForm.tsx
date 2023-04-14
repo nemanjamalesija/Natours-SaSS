6a593818eab036c514275ed31f98f20049350c61
nemanjamalesija <malesija13@gmail.com>
2023-04-14 21:19:59 +0200

    Finish form style

--- src/sections/BookForm.tsx
+++ src/sections/BookForm.tsx
@@ -33,6 +33,37 @@ const BookForm = () => {
                 </label>
               </div>
             </form>
+            <div className='book-form__group'>
+              <div className='book-form__radio-group'>
+                <input
+                  type='radio'
+                  className='book-form__radio-input'
+                  id='small'
+                  name='size'
+                />
+                <label htmlFor='small' className='book-form__radio-label'>
+                  <span className='book-form__radio-button'></span>
+                  Small tour group
+                </label>
+              </div>
+              <div className='book-form__radio-group'>
+                <input
+                  type='radio'
+                  className='book-form__radio-input'
+                  id='large'
+                  name='size'
+                />
+                <label htmlFor='large' className='book-form__radio-label'>
+                  <span className='book-form__radio-button'></span>
+                  Large tour group
+                </label>
+              </div>
+              <div className='book-form__group'>
+                <button type='submit' className='button btn btn--green'>
+                  Next step &rarr;
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
