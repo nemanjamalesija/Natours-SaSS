ef04e84df984f4f6fa1ab9173745d6989463e7ae
nemanjamalesija <malesija13@gmail.com>
2023-04-15 02:34:19 +0200

    Add nav animation

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import About from './sections/About';
 import BookForm from './sections/BookForm';
 import Features from './sections/Features';
 import Footer from './sections/Footer';
+import Navigation from './sections/Navigation';
 import Testimonials from './sections/Testimonials';
 import Tours from './sections/Tours';
 
 function App() {
   return (
     <div className='App'>
+      <Navigation />
       <Header />
       <About />
       <Features />
